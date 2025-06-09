fe797d21eddec19d03780c005b74c186b0811821
Ridwan Setiadi <ridwansetiadi94@gmail.com>
2025-06-09 16:03:47 +0700

    Edit the backend URL in vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5001", // Backend server URL
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  // console.log("VITE_BACKEND_URL:", env.VITE_BACKEND_URL);
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
